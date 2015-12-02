ab4c6c7a5c951ac84377725fcf07e05d68fb4288
Jørgen Lien Sellæg <jorgen@guut.org>
2015-12-02 02:40:05 +0100

    Minor readability change

--- src/python_api.cc
+++ src/python_api.cc
@@ -5,11 +5,13 @@
 
 PYBIND_PLUGIN(libjuci) {
   pybind11::module m("libjuci", "Python API for juCi++");
-  m.def("directories_open", [](const char * dir) {
+
+  m.def("directories_open", [](const char *dir) {
     boost::filesystem::path path(dir);
     if (boost::filesystem::is_directory(path))
       Singleton::directories->open(path);
   }, "Opens directory in file tree", pybind11::arg("String representation of the path"));
+
   m.def("get_highlighted_word", []() {
     auto g_application = g_application_get_default();
     auto gio_application = Glib::wrap(g_application, true);
@@ -27,7 +29,8 @@ PYBIND_PLUGIN(libjuci) {
     }
     return std::string(""); // const char * not viable
   });
-  m.def("get_highlighted_word_type", [] () {
+
+  m.def("get_highlighted_word_type", []() {
     auto g_application = g_application_get_default();
     auto gio_application = Glib::wrap(g_application, true);
     auto application = Glib::RefPtr<Application>::cast_static(gio_application);
@@ -37,13 +40,15 @@ PYBIND_PLUGIN(libjuci) {
         if (view->get_token) {
           auto token = view->get_token();
           return token.type;
-          }
         }
       }
+    }
     return -1;
   });
-  m.def("terminal_print", [](const char * message) {
+
+  m.def("terminal_print", [](const char *message) {
     return Singleton::terminal->print(message);
   });
+  
   return m.ptr();
 }

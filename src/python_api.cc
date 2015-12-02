c994409cd3fe943ef0fbb343a46c545697f7d2cf
Jørgen Lien Sellæg <jorgen@guut.org>
2015-12-02 02:35:34 +0100

    Add more python bindings

--- src/python_api.cc
+++ src/python_api.cc
@@ -1,13 +1,49 @@
 #include "api.h"
 #include <pybind11/pybind11.h>
 #include "singletons.h"
+#include "juci.h"
 
 PYBIND_PLUGIN(libjuci) {
   pybind11::module m("libjuci", "Python API for juCi++");
-  m.def("directories_open", [](const std::string &dir) {
+  m.def("directories_open", [](const char * dir) {
     boost::filesystem::path path(dir);
     if (boost::filesystem::is_directory(path))
       Singleton::directories->open(path);
   }, "Opens directory in file tree", pybind11::arg("String representation of the path"));
+  m.def("get_highlighted_word", []() {
+    auto g_application = g_application_get_default();
+    auto gio_application = Glib::wrap(g_application, true);
+    auto application = Glib::RefPtr<Application>::cast_static(gio_application);
+    if (application) {
+      auto view = application->window->notebook.get_current_view();
+      if (view != nullptr) {
+        if (view->get_token) {
+          auto token = view->get_token();
+          if (token.type != -1) {
+            return token.spelling;
+          }
+        }
+      }
+    }
+    return std::string(""); // const char * not viable
+  });
+  m.def("get_highlighted_word_type", [] () {
+    auto g_application = g_application_get_default();
+    auto gio_application = Glib::wrap(g_application, true);
+    auto application = Glib::RefPtr<Application>::cast_static(gio_application);
+    if (application) {
+      auto view = application->window->notebook.get_current_view();
+      if (view != nullptr) {
+        if (view->get_token) {
+          auto token = view->get_token();
+          return token.type;
+          }
+        }
+      }
+    return -1;
+  });
+  m.def("terminal_print", [](const char * message) {
+    return Singleton::terminal->print(message);
+  });
   return m.ptr();
 }

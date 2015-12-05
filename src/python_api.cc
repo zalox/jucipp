33e660a8d137748ec83c219c54485de6c2781f47
Jørgen Lien Sellæg <jorgen@guut.org>
2015-12-05 15:55:18 +0100

    Add documentation and format api

--- src/python_api.cc
+++ src/python_api.cc
@@ -6,62 +6,77 @@
 PYBIND_PLUGIN(libjuci) {
   pybind11::module m("libjuci", "Python API for juCi++");
 
-  m.def("directories_open", [](const char *dir) {
-    boost::filesystem::path path(dir);
-    if (boost::filesystem::is_directory(path))
-      Singleton::directories->open(path);
-  }, "Opens directory in file tree", pybind11::arg("String representation of the path"));
+  m.def("directories_open",
+    [] (const char *dir) {
+      boost::filesystem::path path(dir);
+      if (boost::filesystem::is_directory(path))
+        Singleton::directories->open(path);
+    },
+    "Returns void. Opens 'dir' in file tree",
+    pybind11::arg("str dir")
+  );
 
-  m.def("get_highlighted_word", []() {
-    auto g_application = g_application_get_default();
-    auto gio_application = Glib::wrap(g_application, true);
-    auto application = Glib::RefPtr<Application>::cast_static(gio_application);
-    if (application) {
-      auto view = application->window->notebook.get_current_view();
-      if (view != nullptr) {
-        if (view->get_token) {
-          auto token = view->get_token();
-          if (token.type != -1) {
-            return token.spelling;
+  m.def("get_highlighted_word",
+    [] () {
+      auto g_application = g_application_get_default();
+      auto gio_application = Glib::wrap(g_application, true);
+      auto application = Glib::RefPtr<Application>::cast_static(gio_application);
+      if (application) {
+        auto view = application->window->notebook.get_current_view();
+        if (view != nullptr) {
+          if (view->get_token) {
+            auto token = view->get_token();
+            if (token.type != -1) {
+              boost::property_tree::ptree ptree;
+              ptree.add("word", token.spelling);
+              ptree.add("type", token.type);
+              std::stringstream ss;
+              boost::property_tree::json_parser::write_json(ss, ptree);
+              std::string res(ss.str());
+              Singleton::terminal->print(res);
+              return res;
+            }
           }
         }
       }
-    }
-    return std::string(""); // const char * not viable
-  });
+      return std::string(""); // const char * not viable
+    },
+    "returns json, empty string on error"
+    "{"
+      "'word': 'word',"
+      "'type': '-1'"
+    "}"
+  );
 
-  m.def("get_highlighted_word_type", []() {
-    auto g_application = g_application_get_default();
-    auto gio_application = Glib::wrap(g_application, true);
-    auto application = Glib::RefPtr<Application>::cast_static(gio_application);
-    if (application) {
-      auto view = application->window->notebook.get_current_view();
-      if (view != nullptr) {
-        if (view->get_token) {
-          auto token = view->get_token();
-          return token.type;
-        }
-      }
-    }
-    return -1;
-  });
+  m.def("terminal_print",
+    [] (const char *message) {
+      return Singleton::terminal->print(message);
+    },
+    "Returns int, Prints 'message' to terminal", //TODO what does the return represent?
+    pybind11::arg("str message, add \n for line break")
+  );
 
-  m.def("terminal_print", [](const char *message) {
-    return Singleton::terminal->print(message);
-  });
-
-  m.def("add_menu_element", [](const char *json) {
-    Singleton::menu->plugin_entries.emplace_back(json);
-  }, "menu_item is menu-descripting string, action is a fully qualified definition");
+  m.def("add_menu_element",
+    [] (const char *json) {
+      Singleton::menu->plugin_entries.emplace_back(json);
+    },
+    "Returns void. Adds menu configured by 'json'",
+    pybind11::arg("str json")
+  );
   
-  m.def("get_project_folder", []() {
-    return Singleton::directories->cmake->project_path.string();
-  }, "gets the current open folder");
+  m.def("get_project_folder",
+    [] () {
+      return Singleton::directories->cmake->project_path.string();
+    },
+    "Returns path of current open directory."
+  );
   
-  m.def("get_juci_home", []() {
-    return Singleton::config->juci_home_path().string();
-  }, "gets juci settings folder");
-  
-
+  m.def("get_juci_home",
+    [] () {
+      return Singleton::config->juci_home_path().string();
+    },
+    "Returns the path of the juci home folder."
+  );
+ 
   return m.ptr();
 }
